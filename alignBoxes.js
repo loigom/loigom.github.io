e1f539cf1ee880f0e4c6cca7a8cf25b99e21ac32
Tanel Loigom <taloig@ttu.ee>
2021-10-24 23:50:16 +0300

    social media links

--- alignBoxes.js
+++ alignBoxes.js
@@ -1,2 +1,12 @@
-let back = document.getElementsByClassName("back")[0]
-back.style.left = window.innerWidth / 2 - back.offsetWidth / 2 + "px";
+const HEIGHT_FROM_TOP_MULTIPLIER = 0.1
+
+let boxes = document.getElementsByClassName("back");
+
+let uppermost_box = document.getElementsByClassName("back")[0]
+uppermost_box.style.left = window.innerWidth / 2 - uppermost_box.offsetWidth / 2 + "px";
+uppermost_box.style.top = window.innerHeight * HEIGHT_FROM_TOP_MULTIPLIER + "px";
+
+for (let i = 1; i < boxes.length; i++) {
+    boxes[i].style.left = window.innerWidth / 2 - boxes[i].offsetWidth / 2 + "px";
+    boxes[i].style.top = parseFloat(uppermost_box.style.top.substr(0, uppermost_box.style.top.length - 2)) + uppermost_box.offsetHeight + "px";
+}
